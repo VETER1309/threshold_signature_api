3875bd3d638b4f6deb81dcc06c23707a2d33dd79
AAweidai <1253464613@qq.com>
2021-10-09 16:51:45 +0800

    Add mast interface of swift

--- swift/Framework/Musig/Musig/musig.swift
+++ swift/Framework/Musig/Musig/musig.swift
@@ -23,26 +23,27 @@ public class Musig{
         return pubkey
     }
     public func getMyReveal(commits:[String], pubkeys:[String]) -> String{
-        if commits.count != pubkeys.count{
-            return "Error"
-        }
+
         musig = reveal_stage(musig, commits.joined(separator: ""), pubkeys.joined(separator: ""))
         return String.init(cString:get_my_reveal(musig))
     }
     public func getMyCosign(reveals:[String], pubkeys:[String]) -> String{
-        if reveals.count != pubkeys.count{
-            return "Error"
-        }
         musig = cosign_stage(musig, reveals.joined(separator: ""), pubkeys.joined(separator: ""))
         return String.init(cString:get_my_cosign(musig))
     }
-    public func getAggSignature(reveals:[String], cosigns:[String], pubkeys:[String]) -> String{
-        if reveals.count != pubkeys.count || reveals.count != cosigns.count{
-            return "Error"
-        }
-        return String.init(cString:get_signature(reveals.joined(separator: ""), pubkeys.joined(separator: ""), cosigns.joined(separator: "")))
-    }
-    public func getAggPublicKey(pubkeys:[String]) -> String{
-        return String.init(cString:get_agg_pubkey(pubkeys.joined(separator: "")))
-    }
+}
+
+public func getAggSignature(reveals:[String], cosigns:[String], pubkeys:[String]) -> String{
+    return String.init(cString:get_signature(reveals.joined(separator: ""), pubkeys.joined(separator: ""), cosigns.joined(separator: "")))
+}
+public func getAggPublicKey(pubkeys:[String]) -> String{
+    return String.init(cString:get_agg_pubkey(pubkeys.joined(separator: "")))
+}
+
+public func generateThresholdPubkey(pubkeys:[String], threshold: UInt8) -> String {
+    return String.init(cString:generate_threshold_pubkey(pubkeys.joined(separator: ""), threshold))
+}
+
+public func generateControlBlock(pubkeys:[String], threshold: UInt8, agg_pubkey: String) -> String {
+    return String.init(cString:generate_control_block(pubkeys.joined(separator: ""), threshold, agg_pubkey))
 }
